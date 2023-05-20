879d723aa7fdc8f1f0e160e1c7658b2ef504a863
BenceX100 <52270269+BenceX100@users.noreply.github.com>
2023-05-20 06:46:35 +0200

    Fix

--- src/main/java/us/simplecraft/VanishPlaceholders/VanishPlaceholders.java
+++ src/main/java/us/simplecraft/VanishPlaceholders/VanishPlaceholders.java
@@ -1,61 +1,30 @@
 package us.simplecraft.VanishPlaceholders;
 
-import com.earth2me.essentials.Essentials;
 import de.myzelyam.api.vanish.VanishAPI;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.kitteh.vanish.VanishManager;
-import org.kitteh.vanish.VanishPlugin;
+import org.bukkit.metadata.MetadataValue;
 
 import java.util.UUID;
 
 public class VanishPlaceholders extends PlaceholderExpansion {
 
-    static String[] plugins = {"SuperVanish", "PremiumVanish", "VanishNoPacket", "Essentials"};
+    static String[] plugins = {"SuperVanish", "PremiumVanish"};
     String pluginName;
-    // Bukkit.getPluginManager().isPluginEnabled("Essentials") || Bukkit.getPluginManager().isPluginEnabled("SuperVanish") || Bukkit.getPluginManager().isPluginEnabled("PremiumVanish") || Bukkit.getPluginManager().isPluginEnabled("VanishNoPacket")
-    private boolean ess, sv, vnp;
-    Essentials essPlugin;
-    VanishManager vanishManager;
+    private boolean sv;
 
     @Override
     public boolean canRegister() {
-        for(String e : plugins ) {
-            if(Bukkit.getPluginManager().getPlugin(e) != null) {
+        for (String e : plugins) {
+            if (Bukkit.getPluginManager().getPlugin(e) != null) {
                 pluginName = e;
-                if(pluginName.equals("VanishNoPacket")) {
-                    if(((VanishPlugin) Bukkit.getPluginManager().getPlugin("VanishNoPacket")).getManager() != null) {
-                        return init();
-                    }
-                    else return false;
-                }
                 return init();
             }
         }
         Bukkit.getLogger().warning("Vanish Placeholders - Could not find a supported plugin!");
         return false;
     }
-
-    /*@Override
-    public boolean register() {
-        if (!canRegister()) {
-            return false;
-        }
-        /*new BukkitRunnable() {
-            @Override
-            public void run() {
-                init();
-            }
-        }.runTaskLater(Bukkit.getPluginManager().getPlugin("PlaceholderAPI"), 1);*/
-        /*init();
-        if(sv || vnp || ess) {
-            return super.register();
-        }*/
-        /*return init();
-        //PlaceholderAPI.registerPlaceholderHook(getIdentifier(), this);
-    }*/
 
     @Override
     public String getIdentifier() {
@@ -69,88 +38,65 @@ public class VanishPlaceholders extends PlaceholderExpansion {
 
     @Override
     public String getVersion() {
-        return "1.2";
+        return "2.0";
     }
 
     @Override
     public String onPlaceholderRequest(Player p, String identifier) {
-        if(identifier.equals("count")) {
-            return "" + vanishedPlayers(p);
+        if (identifier.equals("count")) {
+            return String.valueOf(canSee(p));
         }
-        if(identifier.equals("playercount")) {
-            return "" + (Bukkit.getOnlinePlayers().size() - vanishedPlayers(p));
+        if (identifier.equals("playercount")) {
+            return String.valueOf(Bukkit.getOnlinePlayers().size() - vanishedPlayers(p));
         }
-        if(identifier.equals("vanished")) {
-            return "" + isVanished(p);
+        if (identifier.equals("vanished")) {
+            return String.valueOf(isVanished(p));
         }
         return null;
     }
 
-    private boolean isVanished(Player p) {
-        if(ess)
-            return essPlugin.getVanishedPlayers().contains(p.getName());
-        if(sv) {
-            return VanishAPI.isInvisible(p);
-        }
-        if(vnp) {
-            return vanishManager.isVanished(p);
+    private boolean isVanished(Player player) {
+        for (MetadataValue meta : player.getMetadata("vanished")) {
+            if (meta.asBoolean()) return true;
         }
         return false;
     }
 
     private int vanishedPlayers(Player p) {
-        if(ess) {
-            return essPlugin.getVanishedPlayers().size();
-        }
-        if(sv) {
+        if (sv) {
             int i = 0;
-            for(UUID e : VanishAPI.getInvisiblePlayers()) {
-                if(VanishAPI.canSee(p, Bukkit.getPlayer(e))) {
+            for (UUID e : VanishAPI.getInvisiblePlayers()) {
+                if (Bukkit.getPlayer(e) == null) {
+                    i++;
+                    continue;
+                }
+                if (!VanishAPI.canSee(p, Bukkit.getPlayer(e))) {
                     i++;
                 }
             }
             return i;
         }
-        if(vnp) {
-            return vanishManager.getVanishedPlayers().size();
-        }
-        return -1;
+        return 0;
     }
 
-    /*private boolean init() {
-        if(Bukkit.getPluginManager().isPluginEnabled("SuperVanish") || Bukkit.getPluginManager().isPluginEnabled("PremiumVanish")) {
-            sv = true;
-            return super.register();
+    private int canSee(Player p) {
+        if (sv) {
+            int i = 0;
+            for (UUID e : VanishAPI.getInvisiblePlayers()) {
+                if (Bukkit.getPlayer(e) == null) continue;
+                if (VanishAPI.canSee(p, Bukkit.getPlayer(e))) {
+                    i++;
+                }
+            }
+            return i;
         }
-        if(Bukkit.getPluginManager().isPluginEnabled("VanishNoPacket")) {
-            vnp = true;
-            vanishManager = ((VanishPlugin) Bukkit.getPluginManager().getPlugin("VanishNoPacket")).getManager();
-            return super.register();
-        }
-        if(Bukkit.getPluginManager().isPluginEnabled("Essentials")) {
-            ess = true;
-            essPlugin = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
-            return super.register();
-        }
-        return false;
-    }*/
+        return 0;
+    }
 
     private boolean init() {
-        if(pluginName.equals("SuperVanish") || pluginName.equals("PremiumVanish")) {
+        if (pluginName.equals("SuperVanish") || pluginName.equals("PremiumVanish")) {
             sv = true;
             Bukkit.getLogger().info("Vanish Placeholders - Hooked into SuperVanish or PremiumVanish");
-            return true;
-        }
-        if(pluginName.equals("VanishNoPacket")) {
-            vnp = true;
-            vanishManager = ((VanishPlugin) Bukkit.getPluginManager().getPlugin("VanishNoPacket")).getManager();
-            Bukkit.getLogger().info("Vanish Placeholders - Hooked into VanishNoPacket");
-            return true;
-        }
-        if(pluginName.equals("Essentials")) {
-            ess = true;
-            essPlugin = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
-            Bukkit.getLogger().info("Vanish Placeholders - Hooked into Essentials");
             return true;
         }
         Bukkit.getLogger().warning("Vanish Placeholders - Unable to Hook!");
